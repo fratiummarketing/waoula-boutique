891d91af228f53714ec4fb6fef9c2902be28d781
fratiummarketing <contactfratiummarketing@gmail.com>
2026-08-08 23:19:33 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.WAOULA_CONFIG = Object.freeze({
   supabaseUrl: "https://waugjqmkrigwkiqssbpz.supabase.co",
-  supabaseAnonKey: "sb_publishable_319w--u0wMl-gwtV8zp9cQ_58s3vUhg"
+  supabaseAnonKey: "EJmspvxR_1l6i-_fljkLSCeExmFz-WzGYJxQTBuATV77Kt7KLWgXZxppKJqp4kuWkmxpiYZvFX2nfGbv"
   paypalClientId: "BAAXHxn7VNaLZKqgziw7qiOe0GUISGabYEHq-i6hE4ITidFpxIurxCkpQoz-2eC2KPJGMVHI_PitQzfPFw"
 });
